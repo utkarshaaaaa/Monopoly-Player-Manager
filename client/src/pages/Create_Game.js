a2ab5e8fd164b613ef1f6c06a14efe4886d73dba
utkarshaaaaa <vyasutkarsh11@gmail.com>
2024-08-05 13:33:01 +0530

    commit

--- client/src/pages/Create_Game.js
+++ client/src/pages/Create_Game.js
@@ -77,106 +77,106 @@ export default function Create_Game() {
         <h3> Game ID - {createGameId}</h3>
       </div>
       <div>
-  
         <div className="form__group field">
-        <input
-          type="input"
-          className="form__field"
-          required=""
-          value={playerName}
-          onChange={(e) => {
-            handlePlayerName(e);
-          }}
-          placeholder="name.."
-        />
-        <label for="name" className="form__label">
-          Name
-        </label>
-      </div>
-        
-      </div>
-      <div>
-      <div className="form__group field">
-        <input
-         
-          className="form__field"
-          placeholder="Amount"
-          required=""
-          type="input"
-          value={startingAmount}
-          onChange={(e) => {
-            handleAmount(e);
-          }}
-        />
-        <label for="name" className="form__label">
-          Amount
-        </label>
-      </div>
+          <input
+            type="input"
+            className="form__field"
+            required=""
+            value={playerName}
+            onChange={(e) => {
+              handlePlayerName(e);
+            }}
+            placeholder="name.."
+          />
+          <label for="name" className="form__label">
+            Name
+          </label>
+        </div>
       </div>
       <div>
         <div className="form__group field">
-        <input
-          type="input"
-          className="form__field"
-          required=""
-          value={playerImage}
-          onChange={(e) => {
-            handleImage(e);
-          }}
-          placeholder="URL.."
-        />
-        <label for="name" className="form__label">
-          Image
-        </label>
+          <input
+            className="form__field"
+            placeholder="Amount"
+            required=""
+            type="input"
+            value={startingAmount}
+            onChange={(e) => {
+              handleAmount(e);
+            }}
+          />
+          <label for="name" className="form__label">
+            Amount
+          </label>
+        </div>
       </div>
-        
+      <div>
+        <div className="form__group field">
+          <input
+            type="input"
+            className="form__field"
+            required=""
+            value={playerImage}
+            onChange={(e) => {
+              handleImage(e);
+            }}
+            placeholder="URL.."
+          />
+          <label for="name" className="form__label">
+            Image
+          </label>
+        </div>
       </div>
       <br />
-      
-      <button className="cssbuttons-io-button" onClick={createPlayers} >
-           Create Players
-            <div className="icon">
-              <svg
-                height="24"
-                width="24"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M0 0h24v24H0z" fill="none"></path>
-                <path
-                  d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"
-                  fill="currentColor"
-                ></path>
-              </svg>
-            </div>
-          </button>
+
+      <button className="cssbuttons-io-button" onClick={createPlayers}>
+        Create Players
+        <div className="icon">
+          <svg
+            height="24"
+            width="24"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M0 0h24v24H0z" fill="none"></path>
+            <path
+              d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"
+              fill="currentColor"
+            ></path>
+          </svg>
+        </div>
+      </button>
       <div>
         {created ? <h3>Players Created</h3> : <div></div>}
 
         <div>
           <div className="container">
-            {createdPlayers.map((e, indx) => {
+            {createdPlayers?.map((e, indx) => {
               return (
-                <>
-                  <div className="holder">
-                    <div className="card">
-                      <img
-                        src={e.image}
-                        style={{ width: "auto", height: "auto" }}
-                      />
+                <>{
+                  createPlayers.length!=0 ?<div>No players</div>:<div className="holder">
+                  <div className="card">
+                    <img
+                      className="image-players"
+                      src={e.image}
+                      style={{ width: "auto", height: "auto"}}
+                    />
 
-                      <div className="card__content">
-                        <p className="card__title">Player Details</p>
-                        <p className="card__description">
-                          <ul>
-                            <li> Name : {e.Player_name}</li>
-                            <li>Game ID : {e.game_id}</li>
-                            <li>Amount : ${e.amount}</li>
-                          </ul>
-                        </p>
-                      </div>
+                    <div className="card__content">
+                      <p className="card__title">Player Details</p>
+                      <p className="card__description">
+                        <ul>
+                          <li> Name : {e.Player_name}</li>
+                          <li>Game ID : {e.game_id}</li>
+                          <li>Amount : ${e.amount}</li>
+                        </ul>
+                      </p>
                     </div>
                   </div>
+                </div>
+                  
+                }
+                  
                 </>
               );
             })}
@@ -184,26 +184,28 @@ export default function Create_Game() {
         </div>
       </div>
       <div>
-      <button className="cssbuttons-io-button" onClick={navToJoinPage} >
+        <button className="cssbuttons-io-button" onClick={navToJoinPage}>
           Start Game
-            <div className="icon">
-              <svg
-                height="24"
-                width="24"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M0 0h24v24H0z" fill="none"></path>
-                <path
-                  d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"
-                  fill="currentColor"
-                ></path>
-              </svg>
-            </div>
-          </button>
-        
-      </div>
+          <div className="icon">
+            <svg
+              height="24"
+              width="24"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M0 0h24v24H0z" fill="none"></path>
+              <path
+                d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"
+                fill="currentColor"
+              ></path>
+            </svg>
+          </div>
+        </button>
 
+
+
+       
+      </div>
     </div>
   );
 }
