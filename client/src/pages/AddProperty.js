3295b2ff1b6025afa17cb7e59e1cc896b918f777
utkarshaaaaa <vyasutkarsh11@gmail.com>
2025-02-16 22:18:38 +0530

    commit

--- client/src/pages/AddProperty.js
+++ client/src/pages/AddProperty.js
@@ -1,136 +1,136 @@
-import React, { useState, useContext, useEffect } from "react";
-import axios from "axios";
-import { v4 as uuidv4 } from "uuid";
-import { Link, useLocation, useNavigate } from "react-router-dom";
-import { Data } from "../Context";
+// import React, { useState, useContext } from "react";
+// import axios from "axios";
+// import { useLocation, useNavigate } from "react-router-dom";
+// import { Data } from "../Context";
 
-export default function () {
-  const location = useLocation();
-  const navigate = useNavigate();
-  const PlayerId = location.state.id;
-  const {
-    joined_Players_details,
-    set_joined_Players_details,
-    game_Id,
-    set_game_Id,
-    currentGameId,
-    setCurrentGameId,
-  } = useContext(Data);
+// export default function PropertyForm() {
+//   const location = useLocation();
+//   const navigate = useNavigate();
+//   const PlayerId = location.state.id;
+//   const {
+//     joined_Players_details,
+//     set_joined_Players_details,
+//     game_Id,
+//     set_game_Id,
+//     currentGameId,
+//     setCurrentGameId,
+//   } = useContext(Data);
 
-  const [propertyDetails, setpropertyDetails] = useState({
-    propertyName: "",
-    Rent: "",
-    House1Rent: "",
-    House2Rent: "",
-    House3Rent: "",
-    Hotel: "",
-  });
+//   const [propertyDetails, setPropertyDetails] = useState({
+//     propertyName: "",
+//     Rent: "",
+//     House1Rent: "",
+//     House2Rent: "",
+//     House3Rent: "",
+//     Hotel: "",
+//   });
+//   const [propertyName, setPropertyName] = useState("");
+//   const [rent, setRent] = useState(0);
+//   const [houseRent1, setHouseRent1] = useState(0);
+//   const [houseRent2, setHouseRent2] = useState(0);
+//   const [houseRent3, setHouseRent3] = useState(0);
+//   const [hotel, setHotel] = useState(0);
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setpropertyDetails({
-      ...propertyDetails,
-      [name]: value,
-    });
-  };
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("Form Data Submitted:", propertyDetails);
-  };
+//   const handleChange = (e) => {
+//     const { name, value } = e.target;
+//     setPropertyDetails({
+//       ...propertyDetails,
+//       [name]: value,
+//     });
+//   };
 
-  const addProperty = async () => {
-    await axios
-      .post(
-        `https://monopoly-backend-8omq.onrender.com/players/properties_Buying${PlayerId}`,
-        {
-          property: propertyDetails,
-        }
-      )
-      .then((res) => {
-        console.log(res.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-  const handleViewProperty = () => {
-    navigate("/view_property", { state: { id: PlayerId, test: "test" } });
-  };
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     const propertyDetails = {
+//       propertyName: propertyName,
+//       Rent: rent,
+//       House1Rent: houseRent1,
+//       House2Rent: houseRent2,
+//       House3Rent: houseRent3,
+//       Hotel: hotel,
+//     };
+//     console.log("Form Data Submitted:", propertyDetails);
+//     addProperty();
+//   };
 
-  return (
-    <>
-      <div>
-        <h2> Property Details</h2>
-        {/* <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            name="propertyName"
-            placeholder="Name"
-            value={propertyDetails.propertyName}
-            onChange={handleChange}
-          />
-          <input
-            type="number"
-            name="Rent"
-            placeholder="Rent"
-            value={propertyDetails.Rent}
-            onChange={handleChange}
-          />
-          <input
-            type="number"
-            name="House1Rent"
-            placeholder="House 1"
-            value={propertyDetails.House1Rent}
-            onChange={handleChange}
-          />
-          <input
-            type="number"
-            name="House2Rent"
-            placeholder="House 2"
-            value={propertyDetails.House2Rent}
-            onChange={handleChange}
-          />
-          <input
-            type="number"
-            name="House3Rent"
-            placeholder="House 3"
-            value={propertyDetails.House3Rent}
-            onChange={handleChange}
-          />
-          <input
-            type="number"
-            name="Hotel"
-            placeholder="Hotel"
-            value={propertyDetails.Hotel}
-            onChange={handleChange}
-          />
-          <button type="submit" onClick={()=>{addProperty()}}>Submit</button>
-          
-        </form> */}
-        <div>
-          <h2>Property Input Form</h2>
-          <form onSubmit={handleSubmit}>
-            {Object.keys(propertyDetails).map((key) => (
-              <input
-                key={key}
-                type="text"
-                name={key}
-                placeholder={key}
-                value={propertyDetails[key]}
-                onChange={handleChange}
-              />
-            ))}
-            <button type="submit">Submit</button>
-          </form>
-        </div>
-        <button
-          onClick={() => {
-            handleViewProperty();
-          }}
-        >
-          View Properties
-        </button>
-      </div>
-    </>
-  );
-}
+//   const addProperty = async () => {
+//     try {
+//       const res = await axios.post(
+//         `https://monopoly-backend-8omq.onrender.com/players/properties_Buying${PlayerId}`,
+//         {
+//           property: propertyDetails,
+//         }
+//       );
+//       console.log(res.data);
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   };
+
+//   const handleViewProperty = () => {
+//     navigate("/view_property", { state: { id: PlayerId, test: "test" } });
+//   };
+
+//   return (
+//     <div>
+//       <h2>Property Details</h2>
+//       <div>
+//         Property Name :
+//         <input
+//           value={propertyName}
+//           onChange={(e) => {
+//             setPropertyDetails(e.target.value);
+//           }}
+//         />
+//       </div>
+//       <div>
+//         Rent :
+//         <input
+//           value={rent}
+//           onChange={(e) => {
+//             setRent(e.target.value);
+//           }}
+//         />
+//       </div>
+//       <div>
+//         Rent1 :
+//         <input
+//           value={houseRent1}
+//           onChange={(e) => {
+//             setHouseRent1(e.target.value);
+//           }}
+//         />
+//       </div>
+//       <div>
+//         Rent2 :
+//         <input
+//           value={houseRent2}
+//           onChange={(e) => {
+//             setHouseRent2(e.target.value);
+//           }}
+//         />
+//       </div>
+//       <div>
+//         Rent3 :
+//         <input
+//           value={houseRent3}
+//           onChange={(e) => {
+//             setHouseRent3(e.target.value);
+//           }}
+//         />
+//       </div>
+//       <div>
+//         Hotel :
+//         <input
+//           value={hotel}
+//           onChange={(e) => {
+//             setHotel(e.target.value);
+//           }}
+//         />
+//       </div>
+//       <button onClick={handleSubmit}>Submit Property Details</button>
+
+//       <button onClick={handleViewProperty}>View Properties</button>
+//     </div>
+//   );
+// }
--- client/src/pages/ViewProperty.js
+++ client/src/pages/ViewProperty.js
@@ -1,40 +1,44 @@
-import React, { useState, useContext, useEffect } from "react";
-import { Data } from "../Context";
-import axios from "axios";
-import { Link, useLocation } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+// import React, { useState, useContext, useEffect } from "react";
+// import { Data } from "../Context";
+// import axios from "axios";
+// import { Link, useLocation } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 
-export default function () {
-  const location = useLocation();
+// export default function () {
+//   const location = useLocation();
 
-  const PlayerId = location.state.id;
-  const[propertyDetails,setPropertyDetails]=useState([])
-  useEffect(() => {
-    axios
-      .get(
-        `https://monopoly-backend-8omq.onrender.com/players/getUser_property${PlayerId}`
-      )
-      .then((res) => {
-        console.log(res.data)
-        setPropertyDetails([res.data.property])
-      })
-      .catch((error) => {
-        if (error.message === "Request failed with status code 404") {
-          console.log("error 404");
-        }
-      });
-  }, []);
-  return <>
-  <div>
-    {propertyDetails.map((e)=>{
-        return <div>
-            {e.propertyName}
+//   const PlayerId = location.state.id;
+//   const[propertyDetails,setPropertyDetails]=useState([])
+//   useEffect(() => {
+//     axios
+//       .get(
+//         `https://monopoly-backend-8omq.onrender.com/players/getUser_property${PlayerId}`
+//       )
+//       .then((res) => {
+//         console.log(res.data)
+//         setPropertyDetails([res.data.property])
+//       })
+//       .catch((error) => {
+//         if (error.message === "Request failed with status code 404") {
+//           console.log("error 404");
+//         }
+//       });
+//   }, []);
+//   return <>
+//   <div>
+//     {propertyDetails.length!=0?<div>
 
-        </div>
-    })}
+//       {propertyDetails?.map((e)=>{
+//         return <div>
+//             {e.propertyName}
 
-  </div>
+//         </div>
+//     })}
+//     </div>:<div>Owns No property</div>}
+    
+
+//   </div>
   
   
-  </>;
-}
+//   </>;
+// }
