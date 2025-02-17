259c3300c731548d1bc8d01f419d8bf278822ee8
utkarshaaaaa <vyasutkarsh11@gmail.com>
2025-02-17 14:44:58 +0530

    commit

--- client/src/pages/AddProperty.js
+++ client/src/pages/AddProperty.js
@@ -7,16 +7,7 @@ export default function PropertyForm() {
   const location = useLocation();
   const navigate = useNavigate();
   const PlayerId = location.state.id;
-  const {
-    joined_Players_details,
-    set_joined_Players_details,
-    game_Id,
-    set_game_Id,
-    currentGameId,
-    setCurrentGameId,
-  } = useContext(Data);
 
-  const [propertyDetail, setPropertyDetail] = useState([]);
   const [propertyName, setPropertyName] = useState("");
   const [rent, setRent] = useState(0);
   const [houseRent1, setHouseRent1] = useState(0);
@@ -34,12 +25,12 @@ export default function PropertyForm() {
       const res = await axios.post(
         `https://monopoly-backend-8omq.onrender.com/players/properties_Buying${PlayerId}`,
         {
-          propertyName: propertyName,
-          Rent: rent,
-          House1Rent: houseRent1,
-          House2Rent: houseRent2,
-          House3Rent: houseRent3,
-          Hotel: hotel,
+          propertyName,
+          rent,
+          houseRent1,
+          houseRent2,
+          houseRent3,
+          hotel,
         }
       );
       console.log(res.data);
@@ -53,75 +44,77 @@ export default function PropertyForm() {
   };
 
   return (
-    <div>
-      <h2>Property Details</h2>
+    <>
       <div>
-        Property Name :
-        <input
-          value={propertyName}
-          onChange={(e) => {
-            setPropertyName(e.target.value);
+        <h2>Property Details</h2>
+        <div>
+          Property Name :
+          <input
+            value={propertyName}
+            onChange={(e) => {
+              setPropertyName(e.target.value);
+            }}
+          />
+        </div>
+        <div>
+          Rent :
+          <input
+            value={rent}
+            onChange={(e) => {
+              setRent(e.target.value);
+            }}
+          />
+        </div>
+        <div>
+          Rent1 :
+          <input
+            value={houseRent1}
+            onChange={(e) => {
+              setHouseRent1(e.target.value);
+            }}
+            type="number"
+          />
+        </div>
+        <div>
+          Rent2 :
+          <input
+            value={houseRent2}
+            onChange={(e) => {
+              setHouseRent2(e.target.value);
+            }}
+            type="number"
+          />
+        </div>
+        <div>
+          Rent3 :
+          <input
+            value={houseRent3}
+            onChange={(e) => {
+              setHouseRent3(e.target.value);
+            }}
+            type="number"
+          />
+        </div>
+        <div>
+          Hotel :
+          <input
+            value={hotel}
+            onChange={(e) => {
+              setHotel(e.target.value);
+            }}
+            type="number"
+          />
+        </div>
+        <button
+          onClick={() => {
+            handleSubmit();
           }}
-        />
-      </div>
-      <div>
-        Rent :
-        <input
-          value={rent}
-          onChange={(e) => {
-            setRent(e.target.value);
-          }}
-        />
-      </div>
-      <div>
-        Rent1 :
-        <input
-          value={houseRent1}
-          onChange={(e) => {
-            setHouseRent1(e.target.value);
-          }}
-          type="number"
-        />
-      </div>
-      <div>
-        Rent2 :
-        <input
-          value={houseRent2}
-          onChange={(e) => {
-            setHouseRent2(e.target.value);
-          }}
-          type="number"
-        />
-      </div>
-      <div>
-        Rent3 :
-        <input
-          value={houseRent3}
-          onChange={(e) => {
-            setHouseRent3(e.target.value);
-          }}
-          type="number"
-        />
-      </div>
-      <div>
-        Hotel :
-        <input
-          value={hotel}
-          onChange={(e) => {
-            setHotel(e.target.value);
-          }}
-          type="number"
-        />
-      </div>
-      <button
-        onClick={() => {
-          handleSubmit();
-        }}
-      >
-        Submit Property Details
-      </button>
+        >
+          Submit Property Details
+        </button>
 
-      <button onClick={handleViewProperty}>View Properties</button>
-    </div>
+        <button onClick={handleViewProperty}>View Properties</button>
+      </div>
+    </>
   );
 }
