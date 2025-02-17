365648b218f8ba08f587ac559669873bd275b719
utkarshaaaaa <vyasutkarsh11@gmail.com>
2025-02-17 14:33:44 +0530

    commit

--- client/src/pages/AddProperty.js
+++ client/src/pages/AddProperty.js
@@ -16,7 +16,7 @@ export default function PropertyForm() {
     setCurrentGameId,
   } = useContext(Data);
 
-  const[propertyDetail,setPropertyDetail]=useState([])
+  const [propertyDetail, setPropertyDetail] = useState([]);
   const [propertyName, setPropertyName] = useState("");
   const [rent, setRent] = useState(0);
   const [houseRent1, setHouseRent1] = useState(0);
@@ -26,16 +26,6 @@ export default function PropertyForm() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const propertyDetails = [{
-      propertyName: propertyName,
-      Rent: rent,
-      House1Rent: houseRent1,
-      House2Rent: houseRent2,
-      House3Rent: houseRent3,
-      Hotel: hotel,
-    }];
-    setPropertyDetail([...propertyDetails])
-    console.log("Form Data Submitted:", propertyDetails);
     addProperty();
   };
 
@@ -44,7 +34,12 @@ export default function PropertyForm() {
       const res = await axios.post(
         `https://monopoly-backend-8omq.onrender.com/players/properties_Buying${PlayerId}`,
         {
-          property: propertyDetail,
+          propertyName: propertyName,
+          Rent: rent,
+          House1Rent: houseRent1,
+          House2Rent: houseRent2,
+          House3Rent: houseRent3,
+          Hotel: hotel,
         }
       );
       console.log(res.data);
@@ -85,6 +80,7 @@ export default function PropertyForm() {
           onChange={(e) => {
             setHouseRent1(e.target.value);
           }}
+          type="number"
         />
       </div>
       <div>
@@ -94,6 +90,7 @@ export default function PropertyForm() {
           onChange={(e) => {
             setHouseRent2(e.target.value);
           }}
+          type="number"
         />
       </div>
       <div>
@@ -103,6 +100,7 @@ export default function PropertyForm() {
           onChange={(e) => {
             setHouseRent3(e.target.value);
           }}
+          type="number"
         />
       </div>
       <div>
@@ -112,9 +110,16 @@ export default function PropertyForm() {
           onChange={(e) => {
             setHotel(e.target.value);
           }}
+          type="number"
         />
       </div>
-      <button onClick={handleSubmit}>Submit Property Details</button>
+      <button
+        onClick={() => {
+          handleSubmit();
+        }}
+      >
+        Submit Property Details
+      </button>
 
       <button onClick={handleViewProperty}>View Properties</button>
     </div>
